3f786102ea551d02848a72d3988a4d46d4cf965d
Скрябин Павел Николаевич <skryabin.p@coral-meat.ru>
2023-08-23 14:22:39 +0300

    done

---
+++ README.md
@@ -0,0 +1,38 @@
+# Easy bitlink
+
+This is a little script for creating short link on `bit.ly` and getting click counts for your bitlinks.
+
+### Requirements
+
+Python3 should be already installed. Then use pip (or pip3, if there is a conflict with Python2) to install dependencies:
+
+```bash
+pip install -r requirements.txt
+```
+### Environment variables
+- TOKEN
+1. Create `.env` file near script.py
+2. Write there `TOKEN=YOUR_TOKEN_FROM_BITLY` and save
+
+## Run
+
+Launch on Linux(Python 3) or Windows:
+
+```bash
+$ python script.py
+```
+
+You will see:
+```
+$ Enter your url address:https://bit.ly/3E7rHjX
+```
+
+Enter either an url you want to be short or your `bit.ly` link to get clicks number 
+
+You will see:
+```
+Enter your url address: https://bit.ly/3E7rHjX
+Total clicks on https://bit.ly/3E7rHjX is 2
+```
+
+
---
+++ script.py
@@ -0,0 +1,55 @@
+import requests
+import json
+from urllib.parse import urlparse
+import os
+from dotenv import load_dotenv
+
+def short_link(token:str,link:str):
+    header = {'Authorization': f'Bearer {token}',
+              'Content-Type': 'application/json'}
+    endpoint = 'v4/bitlinks'
+    api_url = 'https://api-ssl.bitly.com/' + endpoint
+    payload = {"long_url": link}
+    response = requests.post(api_url, headers=header,json=payload)
+    response.raise_for_status()
+    json_data = response.json()
+    bitlink = json_data["link"]
+    return bitlink
+
+def count_clicks(token:str,link:str):
+    header = {'Authorization': f'Bearer {token}'}
+    endpoint = 'v4/bitlinks/'
+    parsed_bitlink = urlparse(link)
+    bitlink = parsed_bitlink.netloc+parsed_bitlink.path
+    api_url = f'https://api-ssl.bitly.com/{endpoint}/{bitlink}/clicks/summary'
+    payload = {"unit": "day", "units": -1}
+    response = requests.get(api_url, headers=header, params=payload)
+    response.raise_for_status()
+    json_data = response.json()
+    clicks_number = json_data['total_clicks']
+    return clicks_number
+
+def is_bitlink(url:str):
+    parsed_url = urlparse(url)
+    if parsed_url.netloc == "bit.ly":
+        return True
+    return False
+
+
+if __name__ == "__main__":
+    load_dotenv()
+    TOKEN = os.environ['TOKEN']
+    link = str(input("Enter your url address:"))
+
+    if is_bitlink(link):
+        try:
+            clicks_nubmer = count_clicks(TOKEN,link)
+            print(f'Total clicks on {link} is {clicks_nubmer}')
+        except requests.exceptions.HTTPError as error:
+            print(f'Remote host returns an error :\n {error}')
+    else:
+        try:
+            bitlink = short_link(TOKEN,link)
+            print("Your Bitlink is",bitlink)
+        except requests.exceptions.HTTPError as error:
+            print(f'Remote host returns an error :\n {error}')
