de0c178296be07b6b85da3fd48f19d6004c338e2
Скрябин Павел Николаевич <skryabin.p@coral-meat.ru>
2023-08-23 14:27:03 +0300

    pep8 check

--- script.py
+++ script.py
@@ -4,19 +4,21 @@ from urllib.parse import urlparse
 import os
 from dotenv import load_dotenv
 
-def short_link(token:str,link:str):
+
+def short_link(token: str, link: str):
     header = {'Authorization': f'Bearer {token}',
               'Content-Type': 'application/json'}
     endpoint = 'v4/bitlinks'
     api_url = 'https://api-ssl.bitly.com/' + endpoint
     payload = {"long_url": link}
-    response = requests.post(api_url, headers=header,json=payload)
+    response = requests.post(api_url, headers=header, json=payload)
     response.raise_for_status()
     json_data = response.json()
     bitlink = json_data["link"]
     return bitlink
 
-def count_clicks(token:str,link:str):
+
+def count_clicks(token: str, link: str):
     header = {'Authorization': f'Bearer {token}'}
     endpoint = 'v4/bitlinks/'
     parsed_bitlink = urlparse(link)
@@ -29,7 +31,8 @@ def count_clicks(token:str,link:str):
     clicks_number = json_data['total_clicks']
     return clicks_number
 
-def is_bitlink(url:str):
+
+def is_bitlink(url: str):
     parsed_url = urlparse(url)
     if parsed_url.netloc == "bit.ly":
         return True
@@ -43,13 +46,13 @@ if __name__ == "__main__":
 
     if is_bitlink(link):
         try:
-            clicks_nubmer = count_clicks(TOKEN,link)
-            print(f'Total clicks on {link} is {clicks_nubmer}')
+            clicks_number = count_clicks(TOKEN, link)
+            print(f'Total clicks on {link} is {clicks_number}')
         except requests.exceptions.HTTPError as error:
             print(f'Remote host returns an error :\n {error}')
     else:
         try:
-            bitlink = short_link(TOKEN,link)
-            print("Your Bitlink is",bitlink)
+            bitlink = short_link(TOKEN, link)
+            print("Your Bitlink is", bitlink)
         except requests.exceptions.HTTPError as error:
             print(f'Remote host returns an error :\n {error}')
